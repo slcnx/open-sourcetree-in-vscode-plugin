e9d20bf76f2caaa6cd35deadf79989ea4570d7c0
songliangcheng <1062670898@qq.com>
2021-12-23 12:03:49 +0800

    本地完成依赖检测和质量扫描

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require('vscode')
 const open = require('open')
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,29 +8,29 @@ const open = require('open')
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "helloworld-minimal-sample" is now active!');
+function activate (context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "helloworld-minimal-sample" is now active!')
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sourcetree', () => {
-        // The code you place here will be executed every time your command is executed
-        open.openApp('sourcetree', { arguments: ["-f", vscode.workspace.rootPath.replace('\\', '/')] })
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand('extension.sourcetree', () => {
+    // The code you place here will be executed every time your command is executed
+    open.openApp('sourcetree', { arguments: ['-f', vscode.workspace.rootPath.replace('\\', '/')] })
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Hello World!')
+  })
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
 
 // this method is called when your extension is deactivated
-function deactivate() { }
+function deactivate () { }
 
 // eslint-disable-next-line no-undef
 module.exports = {
-    activate,
-    deactivate
+  activate,
+  deactivate
 }
